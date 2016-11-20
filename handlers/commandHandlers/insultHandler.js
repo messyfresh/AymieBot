7b688f08f9645f2722a868d1573119cf4eaecc15
Carlton Nix <messyfresh@gmail.com>
2016-11-20 15:24:37 -0600

    Updated for ES6

--- handlers/commandHandlers/insultHandler.js
+++ handlers/commandHandlers/insultHandler.js
@@ -1,14 +1,14 @@
 // Generates an insult
 // var debug = require('debug')('aymiebot:insult')
-var commandHandlerHelper = require('./commandHandlerHelpers')
-var insults = require('../../constants/insultList')
+let commandHandlerHelper = require('./commandHandlerHelpers')
+let insults = require('../../constants/insultList')
 
 // Insult someone (using a mention)
 function insultHandler (msg) {
   // Get Mention
-  var msgMentions = commandHandlerHelper.getFirstMention(msg)
+  let msgMentions = commandHandlerHelper.getFirstMention(msg)
   // Generate number between 0 and length of insult array and send the message
-  genInsult(insults.generic, msgMentions, function (result) {
+  genInsult(insults.generic, msgMentions, result => {
     msg.channel.sendMessage(result)
   })
 }
@@ -16,8 +16,8 @@ function insultHandler (msg) {
 // Generate an insult
 // TODO change function arguments into a context so not all arguments are required to be passed
 function genInsult (insultList, mentions, callback) {
-  commandHandlerHelper.randomInt(0, insultList.length, function (result) {
-    var mentionedInsult = insultList[result].replace('$mention', mentions)
+  commandHandlerHelper.randomInt(0, insultList.length, result => {
+    let mentionedInsult = insultList[result].replace('$mention', mentions)
     callback(mentionedInsult)
   })
 }
