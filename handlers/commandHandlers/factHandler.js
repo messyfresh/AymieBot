11aa00d0ebd0d9ac47c99af38f18fc166ba1ff18
Carlton Nix <messyfresh@gmail.com>
2016-11-20 15:26:24 -0600

    Updated for ES6

--- handlers/commandHandlers/factHandler.js
+++ handlers/commandHandlers/factHandler.js
@@ -4,7 +4,7 @@ let debug = require('debug')('aymiebot:factHandler')
 let request = require('request')
 
 function factHandler (msg) {
-  request('http://mentalfloss.com/api/1.0/views/amazing_facts.json?limit=1&display_id=xhr&bypass=1', function (error, response, body) {
+  request('http://mentalfloss.com/api/1.0/views/amazing_facts.json?limit=1&display_id=xhr&bypass=1', (error, response, body) => {
     if (!error && response.statusCode === 200) {
       let fact = JSON.parse(body)
       fact = fact[0].fact_body.replace(/<\/?[^>]+(>|$)/g, '')
