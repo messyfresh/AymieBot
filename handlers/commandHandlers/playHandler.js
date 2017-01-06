c4a4aa9cf54b9bc012923cad7d12bcb7e3f4e255
Carlton Nix <messyfresh@gmail.com>
2017-01-05 23:57:01 -0600

    added play nyan to start playing nyancat to annoy rai

---
+++ handlers/commandHandlers/playHandler.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('aymiebot:playHandler')
+const youtubeStream = require('youtube-audio-stream')
+const nyanUrl = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+
+function playHandler (msg) {
+  debug(msg.msgArray)
+  // Check if user is in a voice channel
+  if (getVoiceChannel(msg) === true) {
+    let channel = msg.member.voiceChannel
+    channel.join().then((connection) => {
+      switch (msg.msgArray[2]) {
+        case 'nyan':
+          let nyanStream = youtubeStream(nyanUrl)
+          let intent = connection.playStream(nyanStream, {volume: 0.5})
+          intent.on('start', () => {
+            debug('Playing Youtube audio')
+          })
+          intent.on('end', () => {
+            debug('Finished playing Youtube audio')
+          })
+          break
+        case 'stop':
+          connection.disconnect()
+          break
+      }
+    })
+  } else {
+    debug('no voice channel')
+  }
+}
+
+function getVoiceChannel (msg) {
+  return !!msg.member.voiceChannel
+}
+
+module.exports = {
+  playHandler: playHandler
+}
