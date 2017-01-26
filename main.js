e6a35c192c4beee578465299093546ef06902447
Carlton Nix <messyfresh@gmail.com>
2017-01-25 21:11:38 -0600

    Removed PMX found a better way to monitor http request

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-// PMX
-require('pmx').init({
-  http: true
-})
-
 const Discord = require('discord.js')
-const client = new Discord.Client()
 const conf = require('./conf/conf.json')
 const debug = require('debug')('aymiebot:bot')
 const messageHandler = require('./handlers/messageHandler').handleMessage
 const guildHandler = require('./handlers/guildHandler').guildHandler
+
+const client = new Discord.Client({autoReconnect: true})
 
 require('events').EventEmitter.prototype._maxListeners = 100
 
@@ -20,10 +16,6 @@ client.on('ready', () => {
 client.on('message', msg => {
   // Pass message to handler... to be handled...
   messageHandler(msg)
-})
-
-client.on('disconnect', () => {
-  client.login(conf.discord.bot.token)
 })
 
 client.login(conf.discord.bot.token)
